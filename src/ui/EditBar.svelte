27585a0faaa8ef4167bf09316f0b36e9451210a7
leeonon <v5sollee@gmail.com>
2023-01-30 01:13:58 +0800

    feat: update

---
+++ src/ui/EditBar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts"></script>
+
+<div class="ctj-edit_bar">
+123
+</div>
+
+<style lang="scss">
+	.ctj-edit_bar {
+		height: 3rem;
+		padding: 1rem 0;
+		display: flex;
+		align-items: center;
+		background: transparent;
+	}
+</style>
--- src/views/EditModal/EditModalContent.svelte
+++ src/views/EditModal/EditModalContent.svelte
@@ -2,6 +2,7 @@
 	import type { LanguageType } from '@/ui/Codemirror/lang';
 	import CodeMirror from "@/ui/Codemirror/index.svelte";
 	import TitleBar from '@/ui/TitleBar.svelte';
+	import EditBar from '@/ui/EditBar.svelte';
 
 	let store: any;
 	export let value: string;
@@ -12,18 +13,27 @@
 	}
 </script>
 
-<div>
 	<TitleBar />
-	<CodeMirror
-		lang={lang}
-		doc={value}
-		bind:docStore={store}
-		on:change={changeHandler}
-	/>
-</div>
+	<div class="ctj-edit_background">
+		<EditBar />
+		<CodeMirror
+			lang={lang}
+			doc={value}
+			bind:docStore={store}
+			on:change={changeHandler}
+		/>
+	</div>
 
-<style>
+<style lang="scss">
 	:global(.codemirror) {
 		border: 1px solid #ddd;
+	}
+	.ctj-edit {
+		&_background {
+			padding: 36px;
+			border-radius: .75rem;
+			background-image: linear-gradient(43deg, rgb(65, 88, 208) 0%, rgb(200, 80, 192) 46%, rgb(255, 204, 112) 100%);
+			background-color: rgb(65, 88, 208);
+		}
 	}
 </style>
